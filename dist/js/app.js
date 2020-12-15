3d24c913ba5a0a1d36b635d4db5ce1e1a801dfc3
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-14 23:14:13 -0600

    I have not completed this branch. Faut y revenir et finir

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,22 @@
 "use strict";
-var anchor = document.querySelector("a");
-console.log(anchor.href);
+var Invoice = (function () {
+  function Invoice(c, d, a) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  Invoice.prototype.format = function () {
+    return this.client + " owes " + this.amount + " for " + this.details;
+  };
+  return Invoice;
+})();
+var invoiceOne = new Invoice("Peach", "Castle Infrastructure", 300);
+var invoiceTwo = new Invoice("Bowser", "Weapons Maintainence", 700);
+console.log(invoiceOne, invoiceTwo);
+var invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+console.log(invoices);
 var forme = document.querySelector(".new-item-form");
 console.log(forme.children);
 var type = document.querySelector("#type");
@@ -8,6 +24,6 @@ var toFrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
 forme.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+  e.preventDefault();
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,41 +1,41 @@
-// typescript doesn't have direct access to the index.html page. Therefore, it doesn't know if the element we are fetching for the HTML actually exists. that's why we get this an when we put:
-// const anchor = document.querySelector("a")
+class Invoice {
+	client: string;
+	details: string;
+	amount: number;
 
+	constructor(c: string, d: string, a: number) {
+		this.client = c;
+		this.details = d;
+		this.amount = a;
+	}
 
-// here are some ways to solve this:
+	format() {
+		return `${this.client} owes ${this.amount} for ${this.details}`
+	}
+}
 
-// 1st way is via an if statement to check if the element exists 
-/**
- * const anchor = document.querySelector("a");
- * if (anchor) {
- * 	 console.log(anchor);
- * }
- */
-// 2nd way that is much simpler and cleaner is putting an ! after element declaration. preferred for fetching by using element names. 
+const invoiceOne = new Invoice("Peach", "Castle Infrastructure", 300);
+const invoiceTwo = new Invoice("Bowser", "Weapons Maintainence", 700);
 
-const anchor = document.querySelector("a")!;
-console.log(anchor.href);
+console.log(invoiceOne, invoiceTwo);
 
-// if you're fetching using class names or id names, where it is more ideal to target a specific element, type casting is preferred. So instead of:
-// const form = document.querySelector("form");
+// NEW: I see. only invoices objects that are made via the invoice class schema are allowed. Just like let myStrings: string[] = [] accepts only strings. This one accepts only the objects that are properly instantiated. ie, it has to follow the schema of lines 17 and 18.
+let invoices: Invoice[] = []
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+console.log(invoices);
 
-// with NEW Type Casting, we now do:
 const forme = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(forme.children);
 
-//NOTE: Typecasting serves to specify which type of element we are selecting with a querySelector. This is why when we hover over the variable, we get the type of the HTML Element. If we don't use Typecasting and select something using a class or id, we get the type of Element instead. Uncomment and hover over line 20 to see this in action.
 
-// grabbing the inputs field using Type Casting
+
 const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-// add event listener to the form.
 forme.addEventListener("submit", (e: Event) => {
 	e.preventDefault();
-	// NEW: amount.value (below) will give as a string.
-	// Try change it to amount.valueAsNumber to change value to number
-	// Ça me sera très utile
 	console.log(type.value, toFrom.value, details.value, amount.valueAsNumber)
 });
