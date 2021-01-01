13e7c650d7dec334ad5bf5ed2d8ecd6e781f6988
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-01-01 15:50:51 -0600

    my commit before merging the code from the 13-modules branch to master

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,15 +1,15 @@
 "use strict";
 var Invoice = (function () {
-  function Invoice(c, d, a) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-  Invoice.prototype.format = function () {
-    return this.client + " owes " + this.amount + " for " + this.details;
-  };
-  return Invoice;
-})();
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
 var invoiceOne = new Invoice("Peach", "Castle Infrastructure", 300);
 var invoiceTwo = new Invoice("Bowser", "Weapons Maintainence", 700);
 console.log(invoiceOne, invoiceTwo);
@@ -24,6 +24,6 @@ var toFrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
 forme.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
